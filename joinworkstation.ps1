f086cbcf11e9926e6908c2abd36ec2700c54d4e3
euge900 <72277420+euge900@users.noreply.github.com>
2020-10-15 01:55:07 -0400

    Add files via upload

---
+++ joinworkstation.ps1
@@ -0,0 +1,17 @@
+﻿#First provide new name for this computer.
+$h = Read-Host "What is the new name you wish to rename this computer to?"
+
+
+#Now use the variable $h to rename localhost
+Rename-Computer -NewName "$h" -Force
+
+
+#Here you statically set the DNS address for the server.
+Set-DnsClientServerAddress -InterfaceIndex 5 -ServerAddresses 192.168.214.128
+
+
+#And here you you joing the domain as long as server is reponding to ICMP requests.
+if (Test-Connection -ComputerName 192.168.214.128 -Quiet) 
+            {
+               Add-Computer -DomainName "home.com" -Credential "home\administrator"
+                    }
