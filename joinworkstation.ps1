01b2d67a1faa3f40d0ff74537df637313ff38b28
euge900 <72277420+euge900@users.noreply.github.com>
2020-10-15 01:44:50 -0400

    Add files via upload

---
+++ joinworkstation.ps1
@@ -0,0 +1,12 @@
+﻿$h = Read-Host "What is the new name you wish to rename this computer to?"
+
+Rename-Computer -NewName "$h" -Force
+
+
+Set-DnsClientServerAddress -InterfaceIndex 5 -ServerAddresses 192.168.214.128
+
+
+if (Test-Connection -ComputerName 192.168.214.128 -Quiet) 
+            {
+               Add-Computer -DomainName "home.com" -Credential "home\administrator"
+                    }
